b4284686f007a7596eb3af702d15043b931d0c0c
TheLastKey420 <44385454+TheLastKey420@users.noreply.github.com>
2019-01-05 05:09:54 +0800

    Add files via upload

---
+++ scrip+deface.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#version 1.0
+
+#                      MAU NGAPAIN TOT?
+#              RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#                       MALU TOLOL!!!
+#                      KAGA BERMORAL!!
+#                     KELUAR KAMPANG!!!
+#              HARGAI KARYA ORANG LAIN GOBLOK!
+
+#07/07/17
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet The Last Key | lolcat
+
+echo     " _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ "  | lolcat
+echo     " TOOL INDONESIA HACKING TEAM " | lolcat
+echo     "    ┈┈╱▔▏┈┈╱▔╲┈┈╱▔╲┈  " | lolcat
+echo     "    ┈╱╱▏▏┈▕╱▔▏▏▕▕▔▏▏" | lolcat
+echo     "    ┈▏▔┈▏┈┈┈╱╱┈▕▕┈▏▏" | lolcat
+echo     "    ┈▔▔▏▏╭╮╱┈▔▏┈╲▔╱┈  " | lolcat
+echo     "    ┈┈┈▔┈╰╯▔▔▔┈┈┈▔┈┈ " | lolcat
+echo     " facebook          =  https://m.facebook.com/TheLastKey420 " | lolcat
+echo     " github                =  https://github.com/TheLastKey420 " | lolcat
+echo     " youtube             =  https://www.youtube.com/channel/UCXRIrHu50KBD5u0ReKcnXyw " | lolcat
+echo     " _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ " l locat
+figlet Gunakan Dengan Bijak | lolcat
+echo     "    "
+echo     "    "
+
+echo -e "SelamatMenikmatiHeheheTheLastkeySangPeniruCommad" | lolcat
+echo ""
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "1 buat script deface${enda}";
+echo -e "============================" | lolcat
+echo -e $b "2 deface tebax index${endcla}";
+echo -e "============================" | lolcat
+echo -e "╭─[PILIH NOMORNYA]"
+read -p "╰─root@./TheLastKey=" pil;
+
+
+# buat script deface
+
+case $pil in
+1) git clone https://github.com/TheLastKey420/def-sc
+
+
+;;
+
+# deface tebax index
+
+2) git clone https://github.com/TheLastKey420/T-index
+
+
+;;
+
+#EXIT
+
+00)  echo " The Last Key 420 " | lolcat
+echo " ──────────────────────  ─────────────────  ───────────   " | lolcat
+echo " ╔══╗╔══╗╔╗─╔══╗╔═╦═╗  ╔══╗╔══╗╔═╗╔══╗  ╔╦╗╔══╗╔═╦═╗╔══╗ " | lolcat
+echo " ║══╣║╔╗║║║─║╔╗║║║║║║  ╚╗╗║║╔╗║║╬║╚║║╝  ║╔╝║╔╗║║║║║║╚║║╝ " | lolcat
+echo " ╠══║║╠╣║║╚╗║╠╣║║║║║║  ╔╩╝║║╠╣║║╗╣╔║║╗  ║╚╗║╠╣║║║║║║╔║║╗ " | lolcat
+echo " ╚══╝╚╝╚╝╚═╝╚╝╚╝╚╩═╩╝  ╚══╝╚╝╚╝╚╩╝╚══╝  ╚╩╝╚╝╚╝╚╩═╩╝╚══╝ " | lolcat
+echo " ────────────────────  ───────────────  ────────────────  " | lolcat
+echo " ╔══╗─────╔╗──────────╔═╗╔╗────  ╔╗╔╗───────╔╗─╔╗───────  ╔══╗───────────   "  | lolcat
+echo " ╚║║╝╔═╦╗╔╝║╔═╗╔═╦╗╔═╗║═╣╠╣╔═╗─  ║╚╝║╔═╗─╔═╗║╠╗╠╣╔═╦╗╔═╗  ╚╗╔╝╔═╗╔═╗─╔══╗   "  | lolcat
+echo " ╔║║╗║║║║║╬║║╬║║║║║║╩╣╠═║║║║╬╚╗  ║╔╗║║╬╚╗║═╣║═╣║║║║║║║╬║  ─║║─║╩╣║╬╚╗║║║║   "  | lolcat    
+echo " ╚══╝╚╩═╝╚═╝╚═╝╚╩═╝╚═╝╚═╝╚╝╚══╝  ╚╝╚╝╚══╝╚═╝╚╩╝╚╝╚╩═╝╠╗║  ─╚╝─╚═╝╚══╝╚╩╩╝   "  | lolcat
+echo "  ──────────────────────────────    ───────────────────╚═╝  ───────────────  "  | lolcat
+ figlet Thank Atas supportnya kawan | lolcat 
+exit
+;;
+
+*) echo "Sorry, Your choices it's not already [T4T]"
+esac
+done
+done
+
+
